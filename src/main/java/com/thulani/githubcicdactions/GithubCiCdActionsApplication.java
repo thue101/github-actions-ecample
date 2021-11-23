629400e4c783786860a5f8ee5e8085108e1a2e58
twins <thulani_mpofu@icloud.com>
2021-11-23 11:52:33 +0200

    code

---
+++ src/main/java/com/thulani/githubcicdactions/GithubCiCdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.thulani.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCiCdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome To Github Actions Example";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiCdActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thulani/githubcicdactions/GithubCiCdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thulani.githubcicdactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCiCdActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
